2504addcfabe98e0e0700299b899363221f27f4f
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-05 16:01:30 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,46 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library (file_path)
+  file = YAML.load_file(file_path)
+  dictionary = {}
+
+  file.each do |name, emoji_array|
+    dictionary[name] = {:english => {}, :japanese => {}}
+    emoji_array.each_with_index do |emoji, index|
+      if index == 0
+        dictionary[name][:english] = emoji
+      elsif index == 1
+        dictionary[name][:japanese] = emoji
+      end
+    end
+  end
+  dictionary
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoji)
+  dictionary = load_library(file_path)
+  japanese_emoji = ""
+  dictionary.each do |name, value|
+    if emoji == value[:english]
+      return japanese_emoji = value[:japanese]
+    else
+      japanese_emoji = "Sorry, that emoticon was not found"
+    end
+  end
+  return japanese_emoji
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoji)
+  dictionary = load_library(file_path)
+  eng_name = ""
+  dictionary.each do |name, value|
+    if emoji == value[:japanese]
+     return eng_name = name
+    else
+      eng_name = "Sorry, that emoticon was not found"
+    end
+  end
+  return eng_name
 end
